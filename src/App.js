15bef1ad799b03e23fa706a26291e0020ed68ce9
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-07-26 16:49:57 +0900

    Making people array with three objects

--- src/App.js
+++ src/App.js
@@ -4,18 +4,43 @@ import './App.css';
 const App = () =>(<PersonList/>)
 
 const PersonList = () =>{
-    return(
-        <Person></Person>
+    const people = [
+    {
+      img:22,
+      name:'John',
+      job:'Developer'
+    },
+    {
+      img:34,
+      name:'Bob',
+      job:'Designer'
+    },
+    {
+      img:56,
+      name:'Peter',
+      job:'Artist'
+    },
+  ];
+    return ( <section>
+        <Person person={people[0]}/>;
+        <Person person={people[1]}>
+            Hi this is a test text.
+        </Person>;
+        <Person person={people[2]}/>;
+    </section>
     )
 }
-const Person = () =>{
-    const url = "https://randomuser.me/api/portraits/thumb/men/22.jpg";
+const Person = (props) =>{
+    const {img,name,job} = props.person;
+    const {children} = props;
+    const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 
     return ( <div class="person">
     <img src={url}></img>
         <div>
-            <h4>Name</h4>
-            <h4>Job</h4>
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+            {children} 
         </div>
     </div>
     )
