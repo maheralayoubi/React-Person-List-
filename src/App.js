48212ad4bf898734b478c7ad2709164765ad79fe
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-08-01 23:22:18 +0900

    Adding new JS code

--- src/App.js
+++ src/App.js
@@ -21,7 +21,30 @@ const PersonList = () =>{
       job:'Artist'
     },
   ];
+    return ( <section>
+        <Person person={people[0]}/>;
+        <Person person={people[1]}>
+            Hi this is a test text.
+        </Person>;
+        <Person person={people[2]}/>;
+    </section>
+    )
+}
+const Person = (props) =>{
+    const {img,name,job} = props.person;
+    const {children} = props;
+    const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 
+    return ( <div class="person">
+    <img src={url}></img>
+        <div>
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+            {children}
+        </div>
+    </div>
+    )
+}
 
 
 
